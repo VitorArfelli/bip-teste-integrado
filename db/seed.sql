66204dcec73c5c6f1fb7fa0118859f31d895ee92
Vitor_Arfelli <arfelliv@outlook.com>
2026-05-09 16:51:57 -0300

    infra: reestruturação do projeto e configuração docker

--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,3 @@
-INSERT INTO BENEFICIO (NOME, DESCRICAO, VALOR, ATIVO) VALUES
-('Beneficio A', 'Descrição A', 1000.00, TRUE),
-('Beneficio B', 'Descrição B', 500.00, TRUE);
+INSERT INTO BENEFICIO (NOME, DESCRICAO, VALOR_CENTAVOS, ATIVO) VALUES
+('Beneficio A', 'Descrição A', 100000, TRUE),
+('Beneficio B', 'Descrição B', 50000, TRUE);
